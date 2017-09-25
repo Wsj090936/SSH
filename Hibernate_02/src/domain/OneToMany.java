3ff59932a96dce53fb4d29d05447feae231de2c0
wushijia <whalewsj@gmail.com>
2017-09-25 22:04:39 +0800

    2017/9/25

--- Hibernate_02/src/domain/OneToMany.java
+++ Hibernate_02/src/domain/OneToMany.java
@@ -1,5 +1,8 @@
 package domain;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.junit.Test;
@@ -13,7 +16,7 @@ import Utils.HibernateUtils;
  */
 public class OneToMany {
 	@Test
-	public void fun1(){
+	public void fun1(){//save-update 级联修改、级联保存
 		Session session = HibernateUtils.openSession();
 		Transaction transaction = session.beginTransaction();
 		
@@ -26,15 +29,48 @@ public class OneToMany {
 		Order o2 = new Order();
 		o2.setName("梨");
 		
-		c.getOrders().add(o1);////多余了
-		c.getOrders().add(o2);//多余了
+		c.getOrders().add(o1);//维护关系
+		c.getOrders().add(o2);//维护关系
 		
-		o1.setCustomer(c);
-		o2.setCustomer(c);
+//		o1.setCustomer(c);
+//		o2.setCustomer(c);
 		
 		session.save(c);
-		session.save(o1);
-		session.save(o2);
+//		session.save(o1);
+//		session.save(o2);
+		
+		transaction.commit();
+		session.close();
+	}
+	@Test
+	public void fun2(){
+		Session session = HibernateUtils.openSession();
+		Transaction transaction = session.beginTransaction();
+		
+		Customer customer = session.get(Customer.class, 4);
+		
+		Set<Order> orders = customer.getOrders();
+		
+		for (Order order : orders) {
+			order.setName("哈哈哈");
+		}
+		
+		transaction.commit();//当设置了级联保存save-update，commit时会自动保存Order的变化
+		session.close();
+	}
+	@Test
+	public void fun3(){//delete-orphan：孤儿删除
+		Session session = HibernateUtils.openSession();
+		Transaction transaction = session.beginTransaction();
+		
+		Customer customer = session.get(Customer.class, 7);
+		
+		Set<Order> orders = customer.getOrders();
+		Iterator<Order> iterator = orders.iterator();
+		while(iterator.hasNext()){
+			iterator.next();
+			iterator.remove();
+		}
 		
 		transaction.commit();
 		session.close();
